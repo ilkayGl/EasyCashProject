8e766dc3648e9645f17be177f1c42541be05e354
ilkay gül <ilkaygl@outlook.com>
2023-05-29 14:14:56 +0300

    version 1

---
+++ EasyCash.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,21 @@
+﻿using EasyCash.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCash.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=.\\SQLEXPRESS;initial catalog=EasyCashDB;integrated security=true");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccoutProcess> CustomerAccoutProcesses { get; set; }
+    }
+}
---
+++ EasyCash.EntityLayer/Concrete/AppUser.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCash.EntityLayer.Concrete
+{
+    public class AppUser : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        [NotMapped]
+        public string FullName => Name + " " + Surname;
+
+        public string Districk { get; set; }
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+
+        public List<CustomerAccount> CustomerAccounts { get; set; }
+    }
+}
